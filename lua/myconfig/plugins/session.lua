7603501bd52ba7a65c210e6dee0b87c4b7332f81
Stancu Florin <niflostancu@gmail.com>
2022-09-01 10:29:56 +0300

    session: fix winminheight error when loading saved session

--- lua/myconfig/plugins/session.lua
+++ lua/myconfig/plugins/session.lua
@@ -33,20 +33,14 @@ lvimPlugin({
       hooks = {
         before_save = function(name) return {} end,
         after_save = function(name, user_data, aborted) end,
-        before_load = function(name, user_data) return user_data end,
+        before_load = function(name, user_data)
+          -- vim.o.winminheight = 0
+          return user_data
+        end,
         after_load = function(name, user_data) end,
       },
       plugins = {
-        close_windows = {
-          hooks = {'before_save', 'before_load'},
-          preserve_layout = true,
-          match = {
-            floating = true,
-            buftype = {},
-            filetype = {},
-            custom = false,  -- or fun(win): boolean
-          },
-        },
+        close_windows = false,
         delete_hidden_buffers = false,
         nvim_tree = true,
         tabby = true,
