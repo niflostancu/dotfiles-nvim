6e59b2f13bdcf5b70365a407d80a675a9d4d76ef
Stancu Florin <niflostancu@gmail.com>
2022-10-28 18:27:20 +0300

    treesitter: add pioasm

--- lua/myconfig/lang/init.lua
+++ lua/myconfig/lang/init.lua
@@ -8,7 +8,7 @@ lvim.builtin.dap.active = true
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
   "bash", "c", "javascript", "json", "lua", "python", "typescript", "tsx",
-  "css", "rust", "java", "yaml",
+  "css", "rust", "java", "yaml", "pioasm",
 }
 lvim.builtin.treesitter.highlight.enabled = true
 -- lvim.builtin.treesitter.ignore_install = { "haskell" }
