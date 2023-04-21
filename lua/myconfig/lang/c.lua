7232c51c3f851cba33dc604c94de61fc3a668096
Stancu Florin <niflostancu@gmail.com>
2023-04-21 12:33:54 +0300

    lang: c, php: avoid init for excluded LSP servers
    (used on low powered devices, e.g. PineBook Pro ARM)

--- lua/myconfig/lang/c.lua
+++ lua/myconfig/lang/c.lua
@@ -3,7 +3,8 @@
 vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "clangd" })
 
 local clangd_flags = {
-  "--query-driver=/usr/bin/arm-none-eabi-gcc"
+  "--query-driver=/usr/bin/arm-none-eabi-gcc",
+  "--enable-config"
 }
 
 local clangd_bin = "clangd"
@@ -19,32 +20,36 @@ local opts = {
   on_attach = custom_on_attach,
 }
 
-require("lvim.lsp.manager").setup("clangd", opts)
+local excludes = vim.tbl_get(lvim.lsp.installer.setup, "automatic_installation", "exclude")
+if not (excludes and vim.tbl_contains(excludes, "clangd")) then
+  require("lvim.lsp.manager").setup("clangd", opts)
 
--- Configure debugging
-local ok, dap_install = pcall(require, 'dap-install')
-if ok then
-  dap_install.config("ccppr_vsc", {
-    adapters = {
-      type = "executable",
-    },
-    configurations = {
-      {
-        type = "cpptools",
-        request = "launch",
-        name = "Launch with pretty-print",
-        program = function()
-          return vim.fn.input('Path to exe: ', vim.fn.getcwd() .. '/', 'file')
-        end,
-        cwd = "${workspaceFolder}",
-        stopOnEntry = true,
-        setupCommands = {
-          {
-            description = "Enable pretty-printing",
-            text = "-enable-pretty-printing",
-          }
-        }
+  -- Configure debugging
+  local ok, dap_install = pcall(require, 'dap-install')
+  if ok then
+    dap_install.config("ccppr_vsc", {
+      adapters = {
+        type = "executable",
       },
-    }
-  })
+      configurations = {
+        {
+          type = "cpptools",
+          request = "launch",
+          name = "Launch with pretty-print",
+          program = function()
+            return vim.fn.input('Path to exe: ', vim.fn.getcwd() .. '/', 'file')
+          end,
+          cwd = "${workspaceFolder}",
+          stopOnEntry = true,
+          setupCommands = {
+            {
+              description = "Enable pretty-printing",
+              text = "-enable-pretty-printing",
+            }
+          }
+        },
+      }
+    })
+  end
+
 end
--- lua/myconfig/lang/php.lua
+++ lua/myconfig/lang/php.lua
@@ -1,8 +1,11 @@
 -- Syntax / LSP configuration for PHP
 
-require("lvim.lsp.manager").setup("intelephense", {
-  init_options = {
-    globalStoragePath = os.getenv('HOME') .. '/.local/share/intelephense'
-  }
-})
+local excludes = vim.tbl_get(lvim.lsp.installer.setup, "automatic_installation", "exclude")
+if not (excludes and vim.tbl_contains(excludes, "intelephense")) then
+  require("lvim.lsp.manager").setup("intelephense", {
+    init_options = {
+      globalStoragePath = os.getenv('HOME') .. '/.local/share/intelephense'
+    }
+  })
+end
 
