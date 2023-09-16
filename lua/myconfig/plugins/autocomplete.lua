e625f34e42d97fb4d6e81e2ac0d97324e04b6038
Stancu Florin <niflostancu@gmail.com>
2023-09-16 11:22:00 +0300

    autocomplete: add ChatGPT plugin

--- lua/myconfig/plugins/autocomplete.lua
+++ lua/myconfig/plugins/autocomplete.lua
@@ -11,3 +11,20 @@ lvimPlugin({
   dependencies = { "ms-jpq/coq_nvim" },
 })
 
+-- Yep, ChatGPT integration
+lvimPlugin({
+  "jackMort/ChatGPT.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("chatgpt").setup({
+        -- api_key_cmd = "secret-tool lookup host https://chat.openai.com",
+        api_key_cmd = "echo 'DISABLED!'"
+      })
+    end,
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim"
+    }
+})
+
