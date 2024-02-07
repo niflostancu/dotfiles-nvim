257323343d48c932a722919f94dbca678eb037af
Stancu Florin <niflostancu@gmail.com>
2024-02-07 10:57:13 +0200

    text: tune width / wrap, add rainbow csv plugin

--- lua/myconfig/lang/text.lua
+++ lua/myconfig/lang/text.lua
@@ -4,3 +4,19 @@ lvimPlugin({
   "nblock/vim-dokuwiki",
 })
 
+lvimPlugin({
+  'cameron-wags/rainbow_csv.nvim',
+  config = true,
+  cmd = {
+    'RainbowDelim',
+    'RainbowDelimSimple',
+    'RainbowDelimQuoted',
+    'RainbowMultiDelim'
+  },
+  filetype = {
+    'csv', 'tsv',
+    'csv_semicolon', 'csv_whitespace',
+    'csv_pipe', 'rfc_csv', 'rfc_semicolon'
+  }
+})
+
