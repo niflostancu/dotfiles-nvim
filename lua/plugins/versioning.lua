99e54c5d6460479c15f4853e79b05344735de400
Stancu Florin <niflostancu@gmail.com>
2024-04-26 14:54:56 +0300

    versioning: configure & use NeoGit as main git wrapper

--- lua/plugins/versioning.lua
+++ lua/plugins/versioning.lua
@@ -4,11 +4,35 @@ return {
   {
     "AstroNvim/astrocommunity",
     { import = "astrocommunity.git.fugit2-nvim" },
+    { import = "astrocommunity.git.neogit" },
+  },
+  {
+    "NeogitOrg/neogit",
+    opts = {
+      kind = "split",
+      commit_select_view = {
+        kind = "auto",
+      },
+      log_view = {
+        kind = "auto",
+      },
+      reflog_view = {
+        kind = "tab",
+      },
+      integrations = {
+        telescope = true,
+        diffview = nil,
+      }
+    },
+    dependencies = {
+      { "nvim-telescope/telescope.nvim" },
+      { "sindrets/diffview.nvim", dependencies = {'nvim-lua/plenary.nvim'} },
+    }
   },
   {
     "AstroNvim/astrocore",
     opts = { mappings = { n = {
-      ["<Leader>gs"] = { "<Cmd>Fugit2<CR>", desc = "Fugit2" },
+      ["<Leader>gs"] = { "<Cmd>Neogit kind=split<CR>", desc = "Neogit Status" },
     } } },
   },
 }
