588dac917c6d9587200ee1bf08fee25df220b848
Stancu Florin <niflostancu@gmail.com>
2024-04-29 15:17:32 +0300

    lang: add community packs for various used langs

---
+++ lua/plugins/lang/init.lua
@@ -0,0 +1,18 @@
+-- Imports community packs for languages
+
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.cpp" },
+  -- { import = "astrocommunity.pack.cmake" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.php" },
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.markdown" },
+}
+
