d202d2f4e85ac9ffa0ac7b76ea4fe5b339b51bef
Stancu Florin <niflostancu@gmail.com>
2024-05-09 17:15:54 +0300

    neo-tree: tweaks: hidden, remove z binding, reveal

--- lua/plugins/explorer.lua
+++ lua/plugins/explorer.lua
@@ -6,9 +6,15 @@ return {
   {
     "nvim-neo-tree/neo-tree.nvim",
     opts = {
+      filesystem = {
+        filtered_items = {
+          visible = true, -- when true, they will just be displayed differently than normal items
+        },
+      },
       window = {
         mappings = {
           H = false, -- preferred for tab cycling
+          z = false, -- prefer default: re-center window
           [">"] = "toggle_hidden",
         },
       },
@@ -19,7 +25,26 @@ return {
         opts = function(_, opts)
           local maps = opts.mappings
           maps.n["<F3>"] = { "<Cmd>Neotree toggle<CR>", desc = "Toggle Explorer" }
-          maps.n[";a"] = { "<Cmd>Neotree filesystem reveal<CR>", desc = "Show in Explorer" }
+          maps.n[";a"] = { function()
+            local reveal_file = vim.fn.expand('%:p')
+            if reveal_file == '' then
+              reveal_file = vim.fn.getcwd()
+            else
+              local f = io.open(reveal_file, "r")
+              if f then
+                f.close(f)
+              else
+                reveal_file = vim.fn.getcwd()
+              end
+            end
+            require('neo-tree.command').execute({
+              action = "focus",          -- OPTIONAL, this is the default value
+              source = "filesystem",     -- OPTIONAL, this is the default value
+              position = "left",         -- OPTIONAL, this is the default value
+              reveal_file = reveal_file, -- path to file or folder to reveal
+              reveal_force_cwd = true,   -- change cwd without asking if needed
+            })
+          end, desc = "Show in Explorer" }
         end
       },
     },
