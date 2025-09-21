a7b9f1f79905c3144c4cd2d1f4f5776e45a90824
Stancu Florin <niflostancu@gmail.com>
2025-09-21 17:48:10 +0300

    lang: add python astrocommunity + whichpy

--- lua/plugins/lang/python.lua
+++ lua/plugins/lang/python.lua
@@ -1,3 +1,11 @@
 return {
   { import = "astrocommunity.pack.python" },
+  {
+    "neolooong/whichpy.nvim",
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+      -- "mfussenegger/nvim-dap-python",
+    },
+    opts = {},
+  },
 }
