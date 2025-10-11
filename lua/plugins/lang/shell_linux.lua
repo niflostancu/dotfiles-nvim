27449345cb3dd03fda4eed3eddb9729427f41d23
Stancu Florin <niflostancu@gmail.com>
2025-10-11 18:47:18 +0300

    lang: workaround for docker lspconfig name error

--- lua/plugins/lang/shell_linux.lua
+++ lua/plugins/lang/shell_linux.lua
@@ -1,5 +1,17 @@
 return {
   { import = "astrocommunity.pack.bash" },
   { import = "astrocommunity.pack.docker" },
+  -- fix docker-language-server not valid in mason-lspconfig
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = function(_, opts)
+      for k,v in ipairs(opts.ensure_installed) do
+        if v == 'docker-language-server' then
+          table.remove(opts.ensure_installed, k)
+          break
+        end
+      end
+    end,
+  },
 }
 
