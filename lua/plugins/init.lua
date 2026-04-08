95dc48cff35ea68e291c117cb89618deb668e6eb
Stancu Florin <niflostancu@gmail.com>
2026-04-08 21:29:31 +0300

    patch: add lazy-local-patcher + sample mcphub fix (unused)

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,6 +1,14 @@
 -- also import plugins from the `plugins.lang` subpackage
 return {
   "AstroNvim/astrocommunity",
-  { import = "plugins.lang" }
+  { import = "plugins.lang" },
+  {
+    "polirritmico/lazy-local-patcher.nvim",
+    enabled = false,
+    opts = {
+      patches_path = _G["myconfigpath"] .. "/patches",
+    },
+    ft = "lazy",
+  }
 }
 
