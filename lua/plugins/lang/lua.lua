20f940836ba23702bb7a9fecbee3daa56a02c202
Stancu Florin <niflostancu@gmail.com>
2026-04-10 22:49:53 +0300

    lang/lua: configure lazydev paths + disable selene LSP

--- lua/plugins/lang/lua.lua
+++ lua/plugins/lang/lua.lua
@@ -1,16 +1,41 @@
 return {
   {
+    "AstroNvim/astrocommunity",
+    { import = "astrocommunity.pack.lua" },
+  },
+  {
     "WhoIsSethDaniel/mason-tool-installer.nvim",
-    opts = {
-      ensure_installed = { "lua-language-server", "prettier", "stylua" },
-    },
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+        "lua-language-server",
+        "stylua",
+        "prettier",
+      })
+      for k,v in ipairs(opts.ensure_installed) do
+        if v == 'selene' then
+          table.remove(opts.ensure_installed, k)
+          break
+        end
+      end
+    end,
   },
   {
     "nvim-treesitter/nvim-treesitter",
     opts = function(_, opts) require("myconfig.utils").ensure_installed(opts, { "lua", "vim" }) end,
   },
+  -- LuaLS config for nvim Lazy dotfiles
   {
-    "AstroNvim/astrocommunity",
-    { import = "astrocommunity.pack.lua" },
+    "folke/lazydev.nvim",
+    ft = "lua",  -- only load on lua files
+    cmd = 'LazyDev',
+    opts = {
+      library = {
+        vim.env.VIMRUNTIME,
+        _G["myconfigpath"] .. "/lua",
+        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+        { path = "snacks.nvim", words = { "Snacks" } },
+        { path = "lazy.nvim", words = { "LazyVim" } },
+      },
+    },
   },
 }
